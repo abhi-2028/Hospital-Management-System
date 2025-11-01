277e21a7e7a32625ab5823d13ebf80ced98a7a70
Abhirup Biswas <abhirupbiswas1clg@gmail.com>
2025-11-01 20:40:25 +0530

    adding files

--- init/data.js
+++ init/data.js
@@ -1,74 +1,76 @@
 const hospitalListings = [
   {
-    name: "Max Super Speciality Hospital",
-    address: "Malsi, Mussoorie Diversion Road, Dehradun, Uttarakhand",
-    contact: "0135-3500000",
-    image:
-      "https://images.unsplash.com/photo-1584982751630-8afc86c4a5c1?auto=format&fit=crop&w=800&q=60",
+    _id: ObjectId('6904d6a16171f9d4ae6bab8c'),
+    name: 'Doon Medical College & Hospital',
+    address: 'Parade Ground, Dehradun, Uttarakhand',
+    contact: '0135-2711177',
+    image: 'https://ik.imagekit.io/syustaging/SYU_PREPROD/Campus-image_knzJgLb69m.webp?tr=w-3840',
+    __v: 0
   },
   {
-    name: "Synergy Institute of Medical Sciences",
-    address: "Ballupur Chowk, Chakrata Road, Dehradun, Uttarakhand",
-    contact: "0135-6672727",
-    image:
-      "https://images.unsplash.com/photo-1623059402394-dc9c28a85c3f?auto=format&fit=crop&w=800&q=60",
+    _id: ObjectId('6904d6a16171f9d4ae6bab8d'),
+    name: 'Combined Medical Institute (CMI Hospital)',
+    address: 'Haridwar Bypass Road, Dehradun, Uttarakhand',
+    contact: '0135-2726692',
+    image: 'https://lh7-us.googleusercontent.com/GFFuIQOakYFDbelsvtVjEzxPjgNRnn7g6xov3SuDDD1eUwNFwZQbhk2Ek0l-XeM_lDix8ZvU8oAqWa71SsR0dQd1aDl6QWPKvvo8D56XouJuw6lURyNqQm3Wpe4k2CYz30u2bJQvT-2lvIHRMoHQhx4',
+    __v: 0
   },
   {
-    name: "Doon Medical College & Hospital",
-    address: "Parade Ground, Dehradun, Uttarakhand",
-    contact: "0135-2711177",
-    image:
-      "https://images.unsplash.com/photo-1603399301333-40f0b5f2e8b7?auto=format&fit=crop&w=800&q=60",
+    _id: ObjectId('6904d6a16171f9d4ae6bab8e'),
+    name: 'Shri Mahant Indiresh Hospital',
+    address: 'Patel Nagar, Dehradun, Uttarakhand',
+    contact: '0135-2743474',
+    image: 'https://best20.in/wp-content/uploads/2023/03/Shri-Mahant-Indiresh-Hospital-Image-768x428.jpg',
+    __v: 0
   },
   {
-    name: "Combined Medical Institute (CMI Hospital)",
-    address: "Haridwar Bypass Road, Dehradun, Uttarakhand",
-    contact: "0135-2726692",
-    image:
-      "https://images.unsplash.com/photo-1580281657529-47d06b8c1b36?auto=format&fit=crop&w=800&q=60",
+    _id: ObjectId('6904d6a16171f9d4ae6bab8f'),
+    name: 'Kailash Hospital',
+    address: 'Haridwar Road, Near ISBT, Dehradun, Uttarakhand',
+    contact: '0135-6675555',
+    image: 'https://doctorlistingingestionpr.blob.core.windows.net/doctorprofilepic/1646459271456_HospitalFacilityImages_1.png',
+    __v: 0
   },
   {
-    name: "Shri Mahant Indiresh Hospital",
-    address: "Patel Nagar, Dehradun, Uttarakhand",
-    contact: "0135-2743474",
-    image:
-      "https://images.unsplash.com/photo-1584433144859-1fc3ab64a957?auto=format&fit=crop&w=800&q=60",
+    _id: ObjectId('6904d6a16171f9d4ae6bab90'),
+    name: 'Himalayan Hospital (Jolly Grant)',
+    address: 'Swami Ram Nagar, Jolly Grant, Dehradun, Uttarakhand',
+    contact: '0135-2471600',
+    image: 'https://www.pioneeredge.in/wp-content/uploads/2021/12/WhatsApp-Image-2021-12-25-at-3.53.26-PM.jpeg',
+    __v: 0
   },
   {
-    name: "Kailash Hospital",
-    address: "Haridwar Road, Near ISBT, Dehradun, Uttarakhand",
-    contact: "0135-6675555",
-    image:
-      "https://images.unsplash.com/photo-1626765734565-4a9e3b8cb0a7?auto=format&fit=crop&w=800&q=60",
+    _id: ObjectId('6904d6a16171f9d4ae6bab91'),
+    name: 'The Hans Foundation Hospital',
+    address: 'Rajpur Road, Jakhan, Dehradun, Uttarakhand',
+    contact: '0135-2658888',
+    image: 'https://www.joonsquare.com/usermanage/image/business/the-hans-foundation-general-hospital-pauri-garhwal-4515/the-hans-foundation-general-hospital-pauri-garhwal-hans7.jpg',
+    __v: 0
   },
   {
-    name: "Himalayan Hospital (Jolly Grant)",
-    address: "Swami Ram Nagar, Jolly Grant, Dehradun, Uttarakhand",
-    contact: "0135-2471600",
-    image:
-      "https://images.unsplash.com/photo-1588776814546-885a6a8b4ba3?auto=format&fit=crop&w=800&q=60",
+    _id: ObjectId('6904d6a16171f9d4ae6bab92'),
+    name: 'Hospital',
+    address: 'Parade Ground, Near Clock Tower, Dehradun, Uttarakhand',
+    contact: '0135-2711177',
+    image: 'https://images.unsplash.com/photo-1580894908361-967195033215?auto=format&fit=crop&w=800&q=60',
+    __v: 0
   },
   {
-    name: "The Hans Foundation Hospital",
-    address: "Rajpur Road, Jakhan, Dehradun, Uttarakhand",
-    contact: "0135-2658888",
-    image:
-      "https://images.unsplash.com/photo-1628352452560-3cc4eaf449eb?auto=format&fit=crop&w=800&q=60",
+    _id: ObjectId('6904d6a16171f9d4ae6bab93'),
+    name: 'Artemis Hospital',
+    address: 'Near IT Park, Sahastradhara Road, Dehradun, Uttarakhand',
+    contact: '0135-3988888',
+    image: 'https://images.unsplash.com/photo-1586773860418-d37222d8fce3?auto=format&fit=crop&w=800&q=60',
+    __v: 0
   },
   {
-    name: "Doon Hospital",
-    address: "Parade Ground, Near Clock Tower, Dehradun, Uttarakhand",
-    contact: "0135-2711177",
-    image:
-      "https://images.unsplash.com/photo-1580894908361-967195033215?auto=format&fit=crop&w=800&q=60",
-  },
-  {
-    name: "Artemis Hospital",
-    address: "Near IT Park, Sahastradhara Road, Dehradun, Uttarakhand",
-    contact: "0135-3988888",
-    image:
-      "https://images.unsplash.com/photo-1586773860418-d37222d8fce3?auto=format&fit=crop&w=800&q=60",
-  },
+    _id: ObjectId('6904ead5096f67f6ef5d5516'),
+    name: 'Hospital of Dehradun',
+    address: 'Selaqui , Dehradun',
+    contact: '9999999999',
+    image: 'https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    __v: 0
+  }
 ];
 
 module.exports = hospitalListings;
